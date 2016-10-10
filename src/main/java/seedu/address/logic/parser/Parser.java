bdbb3a310ed67e08b35d009ab5f0fed4d2eab446
Brandon <brandonyeoxg@gmail.com>
2016-10-10 21:45:16 +0800

    Redone the entire code for non-add-floating-task. to add non floating tasks -> add task from 21 jan 10pm to 22 jan 10pm t/impt

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -224,7 +224,7 @@ public class Parser {
         return new FindCommand(keywordSet);
     }
     
-    private Date getDateFromString(String dateInput) {
+    public static Date getDateFromString(String dateInput) {
         final com.joestelmach.natty.Parser nattyParser = new com.joestelmach.natty.Parser();
         List<DateGroup> dateGroups = nattyParser.parse(dateInput);
         
